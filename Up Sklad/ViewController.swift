9a72a568ddebedfcb622f2bfbb81767cdf690ee6
Blanxxty <Mvtvr21@yandex.ru>
2024-04-30 16:36:06 +0300

    Initial Commit

---
+++ Up Sklad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Up Sklad
+//
+//  Created by Марат Хасанов on 30.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
